39f4fe021722a2298434703055a41957b27f9025
ckdgus988 <ckdgus988@naver.com>
2025-03-16 14:08:46 +0900

    index.ts 파일 생성 구현

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-// index
+import { ContainerRegistry } from './container-registry.class';
+
+// typedi가 동작하가위해 Reflect.getMetadata 필요
+if (!Reflect || !(Reflect as any).getMetadata) {
+  throw new Error('TypeDI requires Reflect.getMetadata to work');
+}
+
+export const Container = ContainerRegistry.defaultContainer;
+export default Container;

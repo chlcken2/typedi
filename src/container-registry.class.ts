af8f803bf3488247494473a7ee98d480d79ea05e
ckdgus988 <ckdgus988@naver.com>
2025-03-16 14:34:51 +0900

    feat: container-registry 함수 클래스 생성 및 default registerContainer 함수 생성

--- src/container-registry.class.ts
+++ src/container-registry.class.ts
@@ -1,1 +1,12 @@
-export class ContainerRegistry {}
+import { ContainerInstance } from './container-instance.class';
+
+export class ContainerRegistry {
+  public static readonly defaultContainer: ContainerInstance = new ContainerInstance('default');
+  public static registerContainer(container: ContainerInstance): void {
+    if (!!ContainerRegistry.defaultContainer && container.id === 'default') {
+      // TODO: Create custom error for this.
+      throw new Error('You cannot register a container with the "default" ID.');
+    }
+    // ContainerRegistry.containerMap.set(container.id, container);
+  }
+}
